2ec823afb83696e18f9b6df4333249262480d330
os-anelapatla <os-anelapatla@projects.site.sa>
2021-02-22 19:12:03 +0530

    test git execution though jenkins

---
+++ Test Suites/Test22022021.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test22022021</description>
+   <name>Test22022021</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>106c4617-0ec8-42a9-a91e-316bc0af1a1a</testSuiteGuid>
+   <testCaseLink>
+      <guid>8d7b6986-0550-4e74-b496-8429570ea35d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UploadDiffFiles_RegisterUser/TC_As a register user, Upload Java File and verify Uploded file in Submission Page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0706d64d-f268-441d-b4c7-5a7831bd688a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UploadDiffFiles_RegisterUser/TC_As a register user, Upload Jar File and verify Uploded file in Submission Page</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
